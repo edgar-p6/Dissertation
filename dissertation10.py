682755a9dea59fd931cbd5ad60853878b132877e
edgar-p6 <99679651+edgar-p6@users.noreply.github.com>
2023-08-27 19:35:10 +0100

    Add files via upload

--- dissertation10.py
+++ dissertation10.py
@@ -6,6 +6,7 @@ Created on Tue Aug 15 19:52:52 2023
 """
 
 import pandas as pd
+import numpy as np
 import re
 import statsmodels.api as sm
 import statsmodels.stats.diagnostic as diag
@@ -25,11 +26,26 @@ def sanitize_variable_name(name):
 sanitized_name_mapping = {var: sanitize_variable_name(var) for var in df_crisis1.columns}
 
 # Extract and define the dependent variable and independent variables
-dependent_variable = 'Sum_GDPcycle'
-independent_variables = df_crisis1.columns.difference([dependent_variable, 'Country', 'Crisis', 'Start', 'End', 'length_db'])
+dependent_variable = 'Mean_diff'
+independent_variables = df_crisis1.columns.difference([dependent_variable, 'Country', 'Crisis', 'Start', 'End', 'length_db', 'Cumulative_diff'])
+
+# Define the independent variables for which to apply the natural logarithm
+variables_to_log = ['Debt service on external debt, total (TDS, current US$)',
+                    'GDP (constant 2015 US$)',
+                    'GDP per capita (constant 2015 US$)',
+                    'Net official aid received (current US$)',
+                    'Official exchange rate (LCU per US$, period average)',
+                    'TRes',
+                    'Use of IMF credit (DOD, current US$)']
+
+#Small constant to add to the variables that will be log-transformed, since some of them have a few values equal to 0
+log_offset = 1e-25
+
+# Remove an outlier identified in one of the independent variables
+df_crisis1.loc[df_crisis1['Short-term debt (% of total reserves)'].idxmax(), 'Short-term debt (% of total reserves)'] = np.nan
 
 # Create a PDF file to save results
-pdf_filename = 'bivariate_regression_results_GDP_1Y.pdf'
+pdf_filename = 'bivariate_regression_results_GDPmean_1Y.pdf'
 
 # Perform bivariate regressions and save to PDF
 with PdfPages(pdf_filename) as pdf:
@@ -38,6 +54,11 @@ with PdfPages(pdf_filename) as pdf:
         subset_df = df_crisis1[[dependent_variable, ind_var]].dropna()
         
         X = sm.add_constant(subset_df[ind_var])  # Add the intercept
+        # Apply natural logarithm with offset to the specific independent variables
+        if ind_var in variables_to_log:
+            subset_df['ln_' + ind_var] = np.log(subset_df[ind_var] + log_offset)
+            X = sm.add_constant(subset_df['ln_' + ind_var])
+        
         y = subset_df[dependent_variable]
         
         model = sm.OLS(y, X).fit()
@@ -45,19 +66,14 @@ with PdfPages(pdf_filename) as pdf:
         # White test for heteroskedasticity
         white_test = diag.het_white(model.resid, exog=model.model.exog)
         
-        # Rainbow test for linearity
-        rainbow_test = diag.linear_rainbow(model)
-        
         # Use a robust estimator for the regressions where heteroskedasticity was found through the White test performed
         if white_test[1] < 0.1:  # Using 0.1 as the threshold, you can adjust this as needed
             model_robust = sm.OLS(y, X).fit(cov_type='HC3')
             summary = model_robust.summary()
             new_white_test = diag.het_white(model_robust.resid, exog=model_robust.model.exog)
-            new_rainbow_test = diag.linear_rainbow(model_robust)
         else:
             summary = model.summary()
             new_white_test = white_test
-            new_rainbow_test = rainbow_test
         
         # Save the summary as a text file
         sanitized_name = sanitized_name_mapping.get(ind_var, sanitize_variable_name(ind_var))
@@ -69,9 +85,6 @@ with PdfPages(pdf_filename) as pdf:
             summary_file.write(f'LM P-Value: {new_white_test[1]}\n')
             summary_file.write(f'F Statistic: {new_white_test[2]}\n')
             summary_file.write(f'F P-Value: {new_white_test[3]}\n')
-            summary_file.write('\nRainbow Test Results:\n')
-            summary_file.write(f'Rainbow Statistic: {new_rainbow_test[0]}\n')
-            summary_file.write(f'Rainbow P-Value: {new_rainbow_test[1]}\n')
         
         # Create a figure and axis for the PDF plot
         fig, ax = plt.subplots(figsize=(8, 10))
@@ -79,14 +92,12 @@ with PdfPages(pdf_filename) as pdf:
         # Add a text annotation to the plot
         ax.text(0.1, 0.9, 'Regression Summary:', fontsize=12)
         ax.text(0.1, 0.88, summary.as_text(), fontsize=10, va='top', ha='left', linespacing=1.5)
-        ax.text(0.1, 0.17, 'White Test Results:', fontsize=12)
-        ax.text(0.1, 0.14, f'LM Statistic: {new_white_test[0]}\nLM P-Value: {new_white_test[1]}\nF Statistic: {new_white_test[2]}\nF P-Value: {new_white_test[3]}', fontsize=10, va='top', ha='left', linespacing=1.5)
-        ax.text(0.8, 0.17, 'Rainbow Test Results:', fontsize=12)
-        ax.text(0.8, 0.14, f'Rainbow Statistic: {new_rainbow_test[0]}\nRainbow P-Value: {new_rainbow_test[1]}', fontsize=10, va='top', ha='left', linespacing=1.5)
+        ax.text(0.1, 0.2, 'White Test Results:', fontsize=12)
+        ax.text(0.1, 0.17, f'LM Statistic: {new_white_test[0]}\nLM P-Value: {new_white_test[1]}\nF Statistic: {new_white_test[2]}\nF P-Value: {new_white_test[3]}', fontsize=10, va='top', ha='left', linespacing=1.5)
         ax.axis('off')
         ax.axis('off')
         
         pdf.savefig(fig, bbox_inches='tight')
         plt.close()
 
-print('Regression results saved to bivariate_regression_results_OLS_1Y.pdf')
+print('Regression results saved to bivariate_regression_results_GDPmean_1Y.pdf')
